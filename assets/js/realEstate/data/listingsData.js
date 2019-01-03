f61781f1e3a8da9491b310d6e0fdc0dfb23fbe56
David Frost <davidmichaelfrost@gmail.com>
2019-01-03 18:35:58 -0500

    Add listingsData.js

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,114 @@
+var listingsdata = [
+  {
+    address: '537 Paper Street',
+    city: 'Accokeek',
+    state: 'MD',
+    rooms: 4,
+    price: 300000,
+    floorSpace: 2500,
+    extras: [
+      'swimming pool',
+      'gym'
+    ],
+    homeType: 'Ranch',
+    image: 'https://www.publicdomainpictures.net/pictures/220000/velka/brick-home.jpg'
+  },
+  {
+    address: '3175 Paper Road',
+    city: 'Baltimore',
+    state: 'MD',
+    rooms: 6,
+    price: 405800,
+    floorSpace: 6300,
+    extras: [
+      'elevator',
+      'garage'
+    ],
+    homeType: 'Neo-eclectic',
+    image: 'https://www.publicdomainpictures.net/pictures/90000/velka/new-home-for-sale-1402850573Fwf.jpg'
+  },
+  {
+    address: '1537 Paper Way',
+    city: 'Rockville',
+    state: 'MD',
+    rooms: 3,
+    price: 640200,
+    floorSpace: 2000,
+    extras: [
+      'swimming pool',
+      'gym'
+    ],
+    homeType: 'Shingle style',
+    image: 'https://www.publicdomainpictures.net/pictures/250000/velka/new-home-construction-1517410003uM8.jpg'
+  },
+  {
+    address: '420 Paper Street',
+    city: 'Baltimore',
+    state: 'MD',
+    rooms: 6,
+    price: 390500,
+    floorSpace: 4500,
+    extras: [
+      'Garage'
+    ],
+    homeType: 'Ranch',
+    image: 'https://www.publicdomainpictures.net/pictures/250000/velka/new-home-construction-1517486635oXU.jpg'
+  },
+  {
+    address: '1537 Paper Street',
+    city: 'Silver Spring',
+    state: 'MD',
+    rooms: 3,
+    price: 503000,
+    floorSpace: 1500,
+    extras: [
+      'elevator',
+      'gym',
+      'swimming pool'
+    ],
+    homeType: 'Prairie style',
+    image: 'https://www.publicdomainpictures.net/pictures/130000/velka/new-home-construction-1437767575Vu6.jpg'
+  },
+  {
+    address: '375 Parchment Street',
+    city: 'Linthicum',
+    state: 'MD',
+    rooms: 4,
+    price: 502000,
+    floorSpace: 3700,
+    extras: [
+      'garage'
+    ],
+    homeType: 'Cape Cod',
+    image: 'https://www.publicdomainpictures.net/pictures/130000/velka/new-home-for-sale-1437771865zHT.jpg'
+  },
+  {
+    address: '735 Paper Way',
+    city: 'Bowie',
+    state: 'MD',
+    rooms: 5,
+    price: 316700,
+    floorSpace: 1900,
+    extras: [
+      'swimming pool'
+    ],
+    homeType: 'Split Foyer',
+    image: 'https://www.publicdomainpictures.net/pictures/100000/velka/new-home-construction-1410089870FZ2.jpg'
+  },
+  {
+    address: '5370 Paper Road',
+    city: 'Columbia',
+    state: 'MD',
+    rooms: 4,
+    price: 370210,
+    floorSpace: 2000,
+    extras: [
+      'garage',
+      'gym'
+    ],
+    homeType: 'Split Foyer',
+    image: 'https://www.publicdomainpictures.net/pictures/130000/velka/new-home-construction-1437771446rYk.jpg'
+  }
+]
+
+export default listingsdata;
