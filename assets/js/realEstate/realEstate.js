0264b31174c8a87e4c5d26f571cfb2518b851cbe
David Frost <davidmichaelfrost@gmail.com>
2019-01-05 22:46:24 -0500

    Add search feature

--- assets/js/realEstate/realEstate.js
+++ assets/js/realEstate/realEstate.js
@@ -27,7 +27,8 @@ class App extends Component {
       filteredData: listingsData,
       populateFormsData: '',
       sortby: 'price-dsc',
-      view: 'long'
+      view: 'box',
+      search: ''
 
     }
 
@@ -93,6 +94,18 @@ class App extends Component {
     if(this.state.sortby == 'price-asc') {
       newData = newData.sort((a,b) => {
         return b.price - a.price
+      })
+    }
+
+    if(this.state.search != '') {
+      newData = newData.filter((item) => {
+        var city = item.city.toLowerCase()
+        var searchText = this.state.search.toLowerCase()
+        var n = city.match(searchText)
+
+        if(n != null) {
+          return true
+        }
       })
     }
 
